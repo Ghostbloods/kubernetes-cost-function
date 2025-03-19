c2c1be22df8634dbbb1265de25c94959f1a64c11
Ryan Goss <ryan.goss@mhc-tn.org>
2025-03-19 15:37:52 -0500

    Fixing Credentials

--- providers.tf
+++ providers.tf
@@ -8,9 +8,3 @@ terraform {
 
   backend "local" {} # We'll update this later for remote state storage
 }
-
-provider "google" {
-  credentials = var.gcp_credentials
-  project     = var.project_id
-  region      = var.region
-}
